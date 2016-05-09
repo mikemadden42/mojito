f82cf939b2b4ddafb97fe39bfcc5df7bbedcd9c6
Michael Madden <mikemadden42@icloud.com>
2016-05-09 10:40:41 -0500

    Initial verison of vpn.pp.

---
+++ manifests/vpn.pp
@@ -0,0 +1,17 @@
+$packages = [
+  'icedtea-7-plugin',
+  'lib32bz2-1.0',
+  'lib32ncurses5',
+  'lib32z1',
+  'libstdc++6:i386',
+  'libxext6:i386'
+  'libxi6:i386'
+  'libxrender1:i386'
+  'libxtst6:i386',
+  'openjdk-7-jre',
+  'openjdk-7-jre:i386'
+]
+
+package { $packages:
+  ensure => 'installed',
+}
